1e83dc869d7858fd2aa671f207233dd6e82b7781
rangel3l2 <rangel.silva@estudante.ifms.edu.br>
2023-09-06 11:26:50 -0400

    add init server

---
+++ startServerEstanteMagica.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+
+# Primeiro diretório
+#!/bin/bash
+
+# Primeiro diretório
+gnome-terminal -- bash -c "cd /text/parceria-ifms-em/desafio4 && echo 'Executando em /text/parceria-ifms-em/desafio4' && go run ."
+
+# Segundo diretório
+gnome-terminal -- bash -c "cd /image/parceria-ifms-em/desafio4 && echo 'Executando em /image/parceria-ifms-em/desafio4' && go run ."
+
